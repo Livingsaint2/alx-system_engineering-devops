14a754878e1afb60912157c100cc41ab841facd6
Livingsaint2 <ofukowoicho2@gmail.com>
2023-08-06 17:39:30 -0700

    Task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,26 @@
-#This pupppet scripts assure an nginx installation
+# install nginx, listen on port 80
+# GET with curl return "Hello World"
+# redirection must a 301 Moved permanently
 
-package {'nginx':
-ensure => present,
-name   => 'nginx',
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+  path    => '/var/www/html/index.html'
+}
+
+file_line { 'title':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true
 }
 
 service { 'nginx':
-ensure     => running,
-enable     => true,
-hasrestart => true,
-require    => Package['nginx'],
-subscribe  => File_line["add redirect"],
-}
-
-file {'/var/www/html/index.html':
-ensure  =>  present,
-path    => '/var/www/html/index.html',
-content => 'Hello World!\n',
-}
-
-file_line {'add redirect':
-ensure  => present,
-path    => '/etc/nginx/sites-available/default',
-replace => true,
-line    => '	rewrite /redirect_me/ https://www.google.com permanent;',
-match   => '# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000',
+  ensure  => running,
+  require => Package['nginx'],
 }
