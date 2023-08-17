37ea56d68ac83ddebb48037474d12d7b4bcdfde3
Livingsaint2 <ofukowoicho2@gmail.com>
2023-08-17 04:33:13 -0700

    Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# a puppet script that remove limit from nginx configuration file
+exec {'remove limit':
+    command => 'mv /etc/default/nginx /etc/default/nginx.bak; sudo service nginx restart',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    onlyif  => 'find /etc/default/nginx'
+}
